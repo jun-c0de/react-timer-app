08f5b6ad24a50ea9d059bafd0f1901bf904e713a
jun-c0de <sh48520414@gmail.com>
2025-08-13 14:56:54 +0900

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Timer from './components/Timer'
+function App() {
+
+
+  return (
+    <div className='app-container'>
+      <Timer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TimerDisplay.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TimerDisplay = ({time,status}) => {
+    return (
+        <div className="timer-display">
+            
+        <div>{time}</div>
+        <p className={`timer-status 
+        ${status==='실행중'? 'running':status==='일시정지'? 'pause':'reset'}`}>
+        {status}
+        </p>
+        </div>
+    )
+}
+
+export default TimerDisplay
